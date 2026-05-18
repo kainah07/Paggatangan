b47267e87d1ca8ea53ff532d55160703444f128e
kai <shekinahjoycanosa7@gmail.com>
2026-05-18 13:13:58 +1200

    stye: change navbar links and icons text color and hover effects

--- paggatangan/src/components/Navbar.jsx
+++ paggatangan/src/components/Navbar.jsx
@@ -8,18 +8,28 @@ function Navbar() {
       <img src={logo} alt="Logo" className='h-16 w-auto'/>
    
       <ul className='flex gap-6'>
-        <li><a href="">Home</a></li>
-        <li><a href="">New</a></li>
-        <li><a href="">Clothing</a></li>
-        <li><a href="">Footwear</a></li>
-        <li><a href="">Accessories</a></li>
+        <li>
+          <a href=""className='text-gray-700 hover:text-black transition'>Home</a>
+          </li>
+        <li>
+          <a href="" className='text-gray-700 hover:text-black transition'>New</a>
+          </li>
+        <li>
+          <a href="" className='text-gray-700 hover:text-black transition'>Clothing</a>
+          </li>
+        <li>
+          <a href="" className='text-gray-700 hover:text-black transition'>Footwear</a>
+          </li>
+        <li>
+          <a href="" className='text-gray-700 hover:text-black transition'>Accessories</a>
+          </li>
       </ul>
 
       <div className='flex gap-3 p-4'>
-        <Search />
-        <Heart />
-        <User />
-        <Handbag />
+        <Search className='text-gray-700 hover:text-black transition' />
+        <Heart className='text-gray-700 hover:text-black transition' />
+        <User className='text-gray-700 hover:text-black transition' />
+        <Handbag className='text-gray-700 hover:text-black transition' />
       </div>
   
     </nav>
